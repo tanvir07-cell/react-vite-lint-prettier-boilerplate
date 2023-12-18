3551865043d1df1295fd887f4f7d76470754fea4
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2023-12-18 10:22:12 +0600

    Chore:for the react boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const App = () => {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+    </div>
+  );
+
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(<App/>);
