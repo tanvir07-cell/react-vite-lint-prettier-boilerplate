05032c714991cb6259d77805056d30976bd0961d
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2023-12-18 10:56:22 +0600

    Chore:for the adding react-hooks pluging added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 
 const App = () => {
+  
   return (
     <div>
       <h2>Hello World!</h2>
