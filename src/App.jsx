c566841c21d72bc39f0aad8672477686c2e73b9d
tanvir07-cell <tr9836859@gmail.com>
2024-02-18 10:31:18 +0600

    Chore:this is the final boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Main from "./components/Tasks/Tasks";
-import TaskController from "./context/TaskController";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <TaskController>
-        <Main />
-      </TaskController>
-
-      <Footer />
-    </>
+    <div className="mx-2 bg-red-500 px-3">
+      <h1>This is the app</h1>
+    </div>
   );
 };
 
